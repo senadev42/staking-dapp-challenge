c6e1bd76619215183bed2bfc7e50831ba68f1d27
senadev42 <2@afrika.dev>
2022-09-22 13:28:47 +0300

    modified external contract

--- packages/hardhat/contracts/ExampleExternalContract.sol
+++ packages/hardhat/contracts/ExampleExternalContract.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 
 contract ExampleExternalContract {
+    //is it completed? this is also a public function
 
-  bool public completed;
+    //a function to flip this from who-knows to yes
+    function complete() public payable {}
 
-  function complete() public payable {
-    completed = true;
-  }
-
+    function returnfunds(address payable _to) public payable {
+        (bool sent, bytes memory data) = _to.call{value: address(this).balance}(
+            ""
+        );
+        require(sent, "Failed to send Ether");
+    }
 }
